23c99e8bc3c13e2cd8b4ac5dbafc90c474e5755a
ameliapeng <apeng@lcs.on.ca>
2020-10-03 11:10:07 -0400

    working on my day five

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -10,3 +10,21 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//function let us reuse code, repeating code is ushually something we avoid, but functions help up avoid doing that. the keyword for using funcion is just "func". it all starts with func, and your function name.
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+
+//we can now use the printhelp() now.
+printHelp()
+
+
+
