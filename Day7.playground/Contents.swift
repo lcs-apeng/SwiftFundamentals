96977e1fa93fbd6be76d4aef27df20910b9c8235
ameliapeng <apeng@lcs.on.ca>
2020-10-06 15:17:21 -0400

    almost done of day seven

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -10,3 +10,65 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//here is an example of what a closure should look like.
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+//then we add our closure.
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+//you also use closures when your returning values as well, for example.
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+//Now when we call "travel()" using trailing closure syntax, our closure code is required to accept a string and return a string:
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+//here is another example about how some closures work.
+
+func travel(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)//the 60 is how many miles youre going, this is very important.
+    print(description)
+    print("I arrived!")
+}
+travel {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+//capturing values can be known as; If you use any external values inside your closure, Swift captures them – stores them alongside the closure, so they can be modified even if they don’t exist any more.
+//here is an example.
+func travel() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+//We can call travel() to get back the closure, then call that closure freely:
+
+let result = travel()
+result("London")
+
+func travel() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+result("London")
+result("London")
+result("London")
+
+
